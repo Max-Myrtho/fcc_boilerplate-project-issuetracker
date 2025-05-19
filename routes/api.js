7571da6d2e4136f94616b5b51a2cb15b83e4ab99
Max-Myrtho <hello@maxmyrtho.me>
2025-05-19 19:33:34 -0400

    trying to understand what to do

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,59 @@
-'use strict';
+"use strict";
+const myDB = require("./connection");
+
+const debug_log = process.env.DEBUG === "true" || false;
+const clog = (l) => debug_log && console.log(l);
+const now = new Date().toISOString();
+
+// {
+//     "_id": "5871dda29faedc3491ff93bb",
+//     "issue_title": "Fix error in posting data",
+//     "issue_text": "When we post data it has an error.",
+//     "created_on": "2017-01-08T06:35:14.240Z",
+//     "updated_on": "2017-01-08T06:35:14.240Z",
+//     "created_by": "Joe",
+//     "assigned_to": "Joe",
+//     "open": true,
+//     "status_text": "In QA"
+//   }
 
 module.exports = function (app) {
+  app
+    .route("/api/issues/:project")
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+    .get(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let project = req.params.project;
-      
+      clog(req.body);
+      myDB(async (client) => {
+        const myDataBase = await client.db("database").collection("projects");
+        clog(myDataBase);
+        //  myDataBase.insertOne(
+        //     {
+        //       username: req.body.username,
+        //       password: hash,
+        //     },
+        //     (err, doc) => {
+        //       if (err) {
+        //         res.redirect("/");
+        //       } else {
+        //         // The inserted document is held within
+        //         // the ops property of the doc
+        //         next(null, doc.ops[0]);
+        //       }
+        //     }
+        //   )
+      });
     })
-    
-    .put(function (req, res){
+
+    .put(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .delete(function (req, res){
+
+    .delete(function (req, res) {
       let project = req.params.project;
-      
     });
-    
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,202 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
-const assert = chai.assert;
-const server = require('../server');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../server");
 
 chai.use(chaiHttp);
+const assert = chai.assert;
 
-suite('Functional Tests', function() {
-  
+suite("Functional Tests", function () {
+  this.timeout(2000);
+
+  const project_name = "chai_project";
+  const now = new Date().toISOString();
+  let updated_issue_id;
+  let deleted_issue_id;
+
+  test("Create an issue with every field: POST request to /api/issues/{project}", (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .post("/api/issues/" + project_name)
+      .send({
+        issue_title: "Test 1",
+        issue_text: "When we post data it has an error.",
+        created_on: now,
+        updated_on: now,
+        created_by: "Chai",
+        assigned_to: "Chai",
+        open: true,
+        status_text: "just testing stuff",
+      })
+      .end((req, res) => {
+        assert.equal(res.status, 201);
+        assert.equal(res.type, "application/json");
+        assert.isNotNull(res.body._id, "res.body is not Null");
+        deleted_issue_id = res.body._id;
+        done();
+      });
+  });
+  test("Create an issue with only required fields: POST request to /api/issues/{project}", (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .post("/api/issues/" + project_name)
+      .send({
+        issue_title: "Test 2",
+        issue_text: "When we post data it has an error.",
+        created_on: now,
+        updated_on: now,
+        created_by: "Chai",
+        open: true,
+      })
+      .end((req, res) => {
+        assert.equal(res.status, 201);
+        done();
+      });
+  });
+  test("Create an issue with missing required fields: POST request to /api/issues/{project}", (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .post("/api/issues/" + project_name)
+      .send({
+        created_on: now,
+        updated_on: now,
+        created_by: "Chai",
+        assigned_to: "Chai",
+        open: true,
+        status_text: "just testing stuff",
+      })
+      .end((req, res) => {
+        assert.equal(res.text, "missing required fields");
+        done();
+      });
+  });
+  test("View issues on a project: GET request to /api/issues/{project}", (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .get("/api/issues/" + project_name)
+      // .send({
+      //   _id: "5871dda29faedc3491ff93bb",
+      // })
+      .end((req, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        done();
+      });
+  });
+  test("View issues on a project with one filter: GET request to /api/issues/{project}", (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .get("/api/issues/" + project_name + "?open=true")
+      .end((req, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        done();
+      });
+  });
+  test("View issues on a project with multiple filters: GET request to /api/issues/{project}", (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .get("/api/issues/" + project_name + "?open=true&assigned_to=Chai")
+      .end((req, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        done();
+      });
+  });
+  test("Update one field on an issue: PUT request to /api/issues/{project}", (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .put("/api/issues/" + project_name)
+      .send({
+        _id: issue_id,
+        issue_title: "Fix error in posting data",
+        issue_text: "When we post data it has an error.",
+        created_on: "2017-01-08T06:35:14.240Z",
+        updated_on: "2017-01-08T06:35:14.240Z",
+        created_by: "Joe",
+        assigned_to: "Joe",
+        open: true,
+        status_text: "In QA",
+      })
+      .end((req, res) => {
+        assert.equal(201, 200);
+        done();
+      });
+  });
+  test("Update multiple fields on an issue: PUT request to /api/issues/{project}", (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .get("/api/issues/" + project_name)
+      .end((req, res) => {
+        assert.equal(201, 200);
+        done();
+      });
+  });
+  test("Update an issue with missing _id: PUT request to /api/issues/{project}", (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .get("/api/issues/" + project_name)
+      .end((req, res) => {
+        assert.equal(201, 200);
+        done();
+      });
+  });
+  test("Update an issue with no fields to update: PUT request to /api/issues/{project}", (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .get("/api/issues/" + project_name)
+      .end((req, res) => {
+        assert.equal(201, 200);
+        done();
+      });
+  });
+  test("Update an issue with an invalid _id: PUT request to /api/issues/{project}", (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .get("/api/issues/" + project_name)
+      .end((req, res) => {
+        assert.equal(201, 200);
+        done();
+      });
+  });
+  test("Delete an issue: DELETE request to /api/issues/{project}", (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .get("/api/issues/" + project_name)
+      .end((req, res) => {
+        assert.equal(201, 200);
+        done();
+      });
+  });
+  test("Delete an issue with an invalid _id: DELETE request to /api/issues/{project}", (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .get("/api/issues/" + project_name)
+      .end((req, res) => {
+        assert.equal(201, 200);
+        done();
+      });
+  });
+  test("Delete an issue with missing _id: DELETE request to /api/issues/{project}", (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .get("/api/issues/" + project_name)
+      .end((req, res) => {
+        assert.equal(201, 200);
+        done();
+      });
+  });
 });
